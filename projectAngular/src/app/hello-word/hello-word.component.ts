3bc3c90a8a4c580dcf2a0d26fa7d6174e4ff3186
Wilson Miranda <intership.natixis@hotmail.com>
2024-11-04 11:36:14 +0000

    add helloWord component

---
+++ projectAngular/src/app/hello-word/hello-word.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-word',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello-word.component.html',
+  styleUrl: './hello-word.component.scss'
+})
+export class HelloWordComponent {
+
+}
