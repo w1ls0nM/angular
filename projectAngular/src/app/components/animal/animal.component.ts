2d57afa7da56293bf735813de657d9d9c74661a2
Wilson Miranda <intership.natixis@hotmail.com>
2024-11-08 09:27:07 +0000

    add class animal

---
+++ projectAngular/src/app/components/animal/animal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal.component.html',
+  styleUrl: './animal.component.scss'
+})
+export class AnimalComponent {
+
+}
